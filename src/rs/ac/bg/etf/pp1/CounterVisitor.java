42b9ac0c59c13c04c28023750d1b67e16a9d6b52
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-07-28 23:41:46 +0200

    Male promene za testiranje.

--- src/rs/ac/bg/etf/pp1/CounterVisitor.java
+++ src/rs/ac/bg/etf/pp1/CounterVisitor.java
@@ -2,28 +2,8 @@ package rs.ac.bg.etf.pp1;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import rs.ac.bg.etf.pp1.ast.*;
 
 public class CounterVisitor extends VisitorAdaptor {
-	
-	protected int count;
-	
-	public int getCount() {
-		return count;
-	}
-	
-	public static class FormParamCounter extends CounterVisitor {
-
-		@Override
-		public void visit(FormalParamDecl formParamDecl1) {
-			count++;
-		}		
-	}
-	
-	public static class VarCounter extends CounterVisitor {		
-		@Override
-		public void visit(VarDecl VarDecl) {
-			count++;
-		}
-	}
 }
