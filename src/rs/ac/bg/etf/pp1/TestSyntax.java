87c91214c4b9c4641bbe7447e08f6ff094ed3664
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-07-28 16:07:28 +0200

    Svasta.

---
+++ src/rs/ac/bg/etf/pp1/TestSyntax.java
@@ -0,0 +1,22 @@
+package rs.ac.bg.etf.pp1;
+
+import java_cup.runtime.Symbol;
+
+import java.io.*;
+
+public class TestSyntax {
+    public static void main(String[] args) throws IOException {
+
+        Reader br = null;
+        File sourceCode = new File("test/moj_program.mj");
+        br = new BufferedReader(new FileReader(sourceCode));
+        Yylex lexer = new Yylex(br);
+
+        Symbol symbol;
+        do {
+            symbol = lexer.next_token();
+            System.out.println(symbol.value);
+
+        } while (symbol.sym != sym.EOF);
+    }
+}
