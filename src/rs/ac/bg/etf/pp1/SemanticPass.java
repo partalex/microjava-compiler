f686a65098bba0639835fee33b5345d3d2c2d24d
Aleksandar Vasilić <48293920+partalex@users.noreply.github.com>
2023-12-11 23:46:57 +0100

    Pre promene SemanticPass i CodeGenerator.

--- src/rs/ac/bg/etf/pp1/SemanticPass.java
+++ src/rs/ac/bg/etf/pp1/SemanticPass.java
@@ -1,14 +1,13 @@
 package rs.ac.bg.etf.pp1;
 
+import org.apache.log4j.Logger;
+import rs.etf.pp1.symboltable.Tab;
+import rs.etf.pp1.symboltable.concepts.Obj;
+import rs.etf.pp1.symboltable.concepts.Struct;
+import rs.ac.bg.etf.pp1.ast.*;
+
 import java.util.ArrayList;
 import java.util.Collection;
-
-import org.apache.log4j.Logger;
-
-
-import rs.ac.bg.etf.pp1.ast.*;
-import rs.etf.pp1.symboltable.Tab;
-import rs.etf.pp1.symboltable.concepts.*;
 
 
 public class SemanticPass extends VisitorAdaptor {
